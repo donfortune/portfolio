5350da80804b39e9be1771de297cd09f17c07bb3
donfortune <donfortunet.df@gmail.com>
2023-04-08 18:52:55 +0100

    added project title and description

---
+++ main.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout='wide')
+
+
+st.image('images/photo.png')
+
+
+st.title('Don-fortune Tangban')
+
+
+content = """ I am a DevOps engineer, Python programmer, and Salesforce administrator with a passion for delivering high-quality software solutions. Don-Fortune is a skilled professional who specializes in designing and implementing DevOps processes that streamline collaboration between development and operations teams.
+With a deep understanding of Python programming, Don-Fortune is well-versed in building robust and scalable software applications that meet the evolving needs of businesses. Their expertise in Salesforce administration also makes them proficient in managing complex CRM implementations that help businesses grow and succeed."""
+st.write(content)
+
+st.write('Below you can find some of the apps i ave built with python. feel free to contact me!')
+
+col1, col2 = st.columns(2)
+data = pandas.read_csv('data.csv', sep=';')
+with col1:
+    for index, row in data[:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image(row['image'])
+        st.write(row['url'])
+
+
+
+
+
+
+with col2:
+    for index, row in data[10:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.write(row['url'])
+
