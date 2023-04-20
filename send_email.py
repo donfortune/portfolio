24270e2510f75d22621c75c3cfb6d811b94bdeaa
Don-Fortune Tangban <74582825+donfortune@users.noreply.github.com>
2023-04-20 08:12:11 +0100

    Configured ssl context

--- send_email.py
+++ send_email.py
@@ -9,7 +9,7 @@ def send_email(message):
     password = 'xxxxxxx'
     receiver_email = 'xxxxxxxxxx@gmail.com'
 
-    context = ssl.create_default_context()  # secure context for sending emails securely
+    context = ssl.create_default_context()  # ssl secure context for sending emails securely
     with smtplib.SMTP_SSL(host, port, context=context) as file:
         file.login(username, password)
         file.sendmail(username, receiver_email, message)
